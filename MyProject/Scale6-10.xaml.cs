3e6bdaced3646dce7bfbbc974ba1281a7500c58e
m01h01 <mheang01@student.rtc.edu>
2023-06-22 00:43:04 -0700

    Initial commit

---
+++ MyProject/Scale6-10.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyProject
+{
+    /// <summary>
+    /// Interaction logic for Scale6_10.xaml
+    /// </summary>
+    public partial class Scale6_10 : Window
+    {
+        public Scale6_10()
+        {
+            InitializeComponent();
+        }
+    }
+}
